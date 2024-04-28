afc98f836fab7d38faee60b949f150d94dc3a37f
Mohammed Mostafa <medo.mostafa22255@gmail.com>
2024-04-29 00:03:06 +0200

    up readme

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.listen(process.env.PORT || port, function () {
     } mode`
   );
 });
-const allowedOrigins = ['http://localhost:3000', 'https://taylor-swift-quotes.onrender.com/']; 
+const allowedOrigins = ['http://localhost:3000', 'https://taylor-swift-quotes.onrender.com/','http://127.0.0.1:5500/']; 
 
 const corsOptions = {
   origin: function (origin, callback) {
