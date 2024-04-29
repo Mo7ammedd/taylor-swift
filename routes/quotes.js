01fc1a3616798b14b1d3b09ce51a96b84e0f481e
Mohammed Mostafa <medo.mostafa22255@gmail.com>
2024-04-30 00:25:38 +0200

    add generate quistion

--- routes/quotes.js
+++ routes/quotes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const Qoute = require('../models/quotesModel');
+const _ = require('lodash');
 const app = express();
 
 // Create quote
@@ -26,5 +27,35 @@ app.get('/all', async (req, res) => {
   const qoute = await Qoute.find();
   res.send(qoute);
 });
+
+//generate quiz questions 
+app.get('/quiz/question', async (req, res) => {
+  try {
+    const randomQuote = await Qoute.aggregate([{ $sample: { size: 1 } }]);
+
+    if (randomQuote.length === 0) {
+      return res.status(404).send({ error: 'No quotes found' });
+    }
+
+    const correctAnswer = randomQuote[0].song; 
+    const question = {
+      question: `${randomQuote[0].quote}`,
+      answers: [
+        correctAnswer, 
+      ...(await getThreeRandomSongs(correctAnswer)), 
+      ],
+    };
+
+    res.send(question);
+  } catch (error) {
+    console.error('Error generating quiz question:', error.message, error.stack);
+    res.status(500).send({ error: 'Failed to generate quiz question' });
+  }
+});
+
+async function getThreeRandomSongs(excludeSong) {
+  const songs = await Qoute.distinct('song', { song: { $ne: excludeSong } });
+  return _.sampleSize(songs, 3); 
+}
 
 module.exports = app;
